87fa0b8d1857c9c1b38fdec325ebe42217472e0b
erkan.yalcin <erkan.yalcin@intra.local>
2018-07-04 18:10:21 +0300

    -web service sample
    https://docs.spring.io/spring-ws/site/reference/html/tutorial.html

--- src/main/java/cc/Application.java
+++ src/main/java/cc/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
-@ComponentScan(basePackages = {"cc"})
+@ComponentScan(basePackages = {"cc","webservices"})
 public class Application {
 	
 	public static void main(String ... args){
